7d96f25ce97fba4eac1b5c3e778896ce8f3adf80
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2015-01-24 17:55:57 +0200

    Added gulp task for moving the first html file to the resources directory.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,21 @@
 var gulp = require('gulp');
 var del = require('del');
 
+var paths = {
+  markup: 'frontend/index.html'
+};
+
 gulp.task('clean', function(cb) {
   del(['resources'], cb);
 });
+
+gulp.task('markup', ['clean'], function() {
+  return gulp.src(paths.markup)
+  .pipe(gulp.dest('resources/'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(paths.markup, ['markup']);
+});
+
+gulp.task('default', ['watch', 'markup']);
