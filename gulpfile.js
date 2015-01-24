fd142105f23fdee3351df01f51d47183ad607ee0
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2015-01-24 19:10:48 +0200

    Removed dependency to clean from markup. Added path variable to destination directory.

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,17 @@ var gulp = require('gulp');
 var del = require('del');
 
 var paths = {
-  markup: 'frontend/index.html'
+  markup: 'frontend/index.html',
+  dest: 'resources/'
 };
 
 gulp.task('clean', function(cb) {
   del(['resources'], cb);
 });
 
-gulp.task('markup', ['clean'], function() {
+gulp.task('markup', function() {
   return gulp.src(paths.markup)
-  .pipe(gulp.dest('resources/'));
+  .pipe(gulp.dest(paths.dest));
 });
 
 gulp.task('watch', function() {
