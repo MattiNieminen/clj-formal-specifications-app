f92e5c2476eb796fffaa79f221994c841d518565
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2015-01-25 12:37:55 +0200

    Added ring-defaults to all routes.

--- src/clj_formal_specifications_app/core.clj
+++ src/clj_formal_specifications_app/core.clj
@@ -1,9 +1,10 @@
 (ns clj-formal-specifications-app.core
   (:require [compojure.core :refer :all]
+            [ring.middleware.defaults :refer :all]
             [org.httpkit.server :refer [run-server]]))
 
 (defroutes all-routes
   (GET "/" [] "Hello World"))
 
 (defn -main []
-  (run-server all-routes {:port 5000}))
+  (run-server (wrap-defaults all-routes site-defaults) {:port 5000}))
