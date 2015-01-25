59705705b70790420d417931b4c3127433bf7d6e
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2015-01-25 17:29:26 +0200

    Updated project clojure dependencies to latest versions.

--- project.clj
+++ project.clj
@@ -5,6 +5,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [compojure "1.1.8"]
-                 [ring/ring-defaults "0.1.1"]
-                 [http-kit "2.1.16"]])
+                 [compojure "1.3.1"]
+                 [ring/ring-defaults "0.1.3"]
+                 [http-kit "2.1.19"]])
