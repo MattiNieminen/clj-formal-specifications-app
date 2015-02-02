8e7d79b93eb10b3f464bed3548864a9428a46ae9
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2015-02-02 21:48:14 +0200

    Implemented a functionality to reset the contents of the editor.

--- frontend/scripts/components.js
+++ frontend/scripts/components.js
@@ -1,23 +1,32 @@
 var SpecificationBox = React.createClass({
+  resetClicked: function() {
+    this.refs.editor.setValue("");
+    this.refs.editor.focus();
+  },
   render: function() {
     return (
       <div id="specificationBox">
-        <Header />
+        <Header onResetClicked={this.resetClicked} />
         <ExecutionBox />
-        <Editor />
+        <Editor ref="editor"/>
       </div>
     );
   }
 });
 
 var Header = React.createClass({
+  handleResetClicked: function() {
+    this.props.onResetClicked();
+  },
   render: function() {
     return (
       <div id="header">
         <ul>
           <li><a href="#compose">Compose</a></li>
           <li><a href="#export">Export</a></li>
-          <li><a href="#reset">Reset editor</a></li>
+          <li>
+            <a href="#reset" onClick={this.handleResetClicked}>Reset editor</a>
+          </li>
           <li><a href="#examples">Examples</a></li>
         </ul>
       </div>
@@ -26,10 +35,21 @@ var Header = React.createClass({
 });
 
 var Editor = React.createClass({
+  getValue: function() {
+    return this.editor.getValue();
+  },
+  setValue: function(value) {
+    this.editor.setValue(value);
+  },
+  focus: function() {
+    this.editor.focus();
+  },
   componentDidMount: function() {
     var editor = ace.edit("editor");
     editor.setTheme("ace/theme/monokai");
     editor.getSession().setMode("ace/mode/clojure");
+    editor.focus();
+    this.editor = editor;
   },
   render: function() {
     return (
