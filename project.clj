1a4ce16829ab48009a178ee4d5a1bdd2ae34c48e
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2015-02-07 21:38:32 +0200

    Added ring-json to project dependencies.

--- project.clj
+++ project.clj
@@ -7,4 +7,5 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.3.1"]
                  [ring/ring-defaults "0.1.3"]
+                 [ring/ring-json "0.3.1"]
                  [http-kit "2.1.19"]])
