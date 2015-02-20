59c893029157c0d1cb26cfc7a60bfe5d4f3aeccb
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2015-02-20 22:48:50 +0200

    Updated versions of project dependencies.

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.3.1"]
-                 [ring/ring-defaults "0.1.3"]
+                 [ring/ring-defaults "0.1.4"]
                  [ring/ring-json "0.3.1"]
                  [http-kit "2.1.19"]
-                 [clj-formal-specifications "1.0.0"]])
+                 [clj-formal-specifications "1.1.0"]])
