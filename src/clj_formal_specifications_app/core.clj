680bfa362687de90af34264f498e5000e0e22bcd
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2015-03-06 12:53:49 +0200

    Moved functions called from the site related routes to their own function.

--- src/clj_formal_specifications_app/core.clj
+++ src/clj_formal_specifications_app/core.clj
@@ -1,10 +1,10 @@
 (ns clj-formal-specifications-app.core
   (:require [clj-formal-specifications-app.api :as api]
+            [clj-formal-specifications-app.site :as site]
             [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.middleware.defaults :refer :all]
             [ring.middleware.json :refer :all]
-            [ring.util.response :as resp]
             [org.httpkit.server :refer [run-server]]))
 
 (defroutes api-routes
@@ -14,14 +14,8 @@
   (POST "/api/execute" [ns command] (api/execute-with-ns ns command))
   (GET "/api/namespace/:ns" [ns] (api/ns-data ns)))
 
-(defn index
-  [req]
-  (resp/content-type
-    (resp/resource-response "index.html" {:root "public"})
-    "text/html"))
-
 (defroutes site-routes
-  (GET "/" [] index)
+  (GET "/" [] site/index)
   (route/resources "/")
   (route/not-found "Not found!"))
 
--- src/clj_formal_specifications_app/site.clj
+++ src/clj_formal_specifications_app/site.clj
@@ -1,1 +1,8 @@
-(ns clj-formal-specifications-app.site)
+(ns clj-formal-specifications-app.site
+  (:require [ring.util.response :as resp]))
+
+(defn index
+  [req]
+  (resp/content-type
+    (resp/resource-response "index.html" {:root "public"})
+    "text/html"))
