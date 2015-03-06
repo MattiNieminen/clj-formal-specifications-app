1d26cccffb281c8a5bfca9298a040c906278acaa
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2015-03-06 13:00:06 +0200

    Moved compojure routes to their own file.

--- src/clj_formal_specifications_app/core.clj
+++ src/clj_formal_specifications_app/core.clj
@@ -1,33 +1,7 @@
 (ns clj-formal-specifications-app.core
-  (:require [clj-formal-specifications-app.api :as api]
-            [clj-formal-specifications-app.site :as site]
-            [compojure.core :refer :all]
-            [compojure.route :as route]
-            [ring.middleware.defaults :refer :all]
-            [ring.middleware.json :refer :all]
+  (:require [clj-formal-specifications-app.routes :as routes]
             [org.httpkit.server :refer [run-server]]))
-
-(defroutes api-routes
-  (GET "/api/examples" [] (api/example-listing))
-  (GET "/api/examples/:filename" [filename] (api/example-file filename))
-  (POST "/api/compose" [specification] (api/compose specification))
-  (POST "/api/execute" [ns command] (api/execute-with-ns ns command))
-  (GET "/api/namespace/:ns" [ns] (api/ns-data ns)))
-
-(defroutes site-routes
-  (GET "/" [] site/index)
-  (route/resources "/")
-  (route/not-found "Not found!"))
-
-(def site (wrap-defaults site-routes site-defaults))
-
-(def api
-  (-> api-routes
-      (wrap-json-response)
-      (wrap-defaults api-defaults)))
-
-(def app (routes api site))
 
 (defn -main
   []
-  (run-server app {:port 5000}))
+  (run-server routes/app {:port 5000}))
--- src/clj_formal_specifications_app/routes.clj
+++ src/clj_formal_specifications_app/routes.clj
@@ -1,1 +1,28 @@
-(ns clj-formal-specifications-app.routes)
+(ns clj-formal-specifications-app.routes
+  (:require [clj-formal-specifications-app.api :as api]
+            [clj-formal-specifications-app.site :as site]
+            [compojure.core :refer :all]
+            [compojure.route :as route]
+            [ring.middleware.defaults :refer :all]
+            [ring.middleware.json :refer :all]))
+
+(defroutes api-routes
+  (GET "/api/examples" [] (api/example-listing))
+  (GET "/api/examples/:filename" [filename] (api/example-file filename))
+  (POST "/api/compose" [specification] (api/compose specification))
+  (POST "/api/execute" [ns command] (api/execute-with-ns ns command))
+  (GET "/api/namespace/:ns" [ns] (api/ns-data ns)))
+
+(defroutes site-routes
+  (GET "/" [] site/index)
+  (route/resources "/")
+  (route/not-found "Not found!"))
+
+(def site (wrap-defaults site-routes site-defaults))
+
+(def api
+  (-> api-routes
+      (wrap-json-response)
+      (wrap-defaults api-defaults)))
+
+(def app (routes api site))
