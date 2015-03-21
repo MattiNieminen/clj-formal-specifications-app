265e66041f3fbfbb970af4d6df6628239a0769fc
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2015-03-21 15:02:21 +0200

    Updated versions of dependencies.

--- project.clj
+++ project.clj
@@ -5,11 +5,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [compojure "1.3.1"]
+                 [compojure "1.3.2"]
                  [ring/ring-defaults "0.1.4"]
                  [ring/ring-json "0.3.1"]
                  [http-kit "2.1.19"]
-                 [clj-formal-specifications "1.1.0"]]
+                 [clj-formal-specifications "1.2.0"]]
   :main ^:skip-aot clj-formal-specifications-app.server
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
