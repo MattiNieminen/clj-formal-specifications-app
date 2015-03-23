c4de186d8d45aaa524dc96c1faaa2b1d2431e420
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2015-03-23 19:23:07 +0200

    Added logging of the url of the front page when the application has been started.

--- src/clj_formal_specifications_app/server.clj
+++ src/clj_formal_specifications_app/server.clj
@@ -1,8 +1,11 @@
 (ns clj-formal-specifications-app.server
   (:gen-class)
   (:require [clj-formal-specifications-app.routes :as routes]
+            [clojure.tools.logging :as log]
             [org.httpkit.server :as httpkit]))
 
 (defn -main
   []
-  (httpkit/run-server routes/app {:port 5000}))
+  (let [port 5000]
+    (httpkit/run-server routes/app {:port port})
+    (log/info (str "Application started at http://localhost:" port))))
