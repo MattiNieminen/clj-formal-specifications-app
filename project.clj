92ec29a4612489285638adf8fb30debd2e599238
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2015-03-23 19:22:15 +0200

    Added logging library to the project.

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/tools.logging "0.3.1"]
                  [compojure "1.3.2"]
                  [ring/ring-defaults "0.1.4"]
                  [ring/ring-json "0.3.1"]
