83642fdc4fc116750b61a1e95c30eb7f7c375576
Matti Nieminen <MattiNieminen@users.noreply.github.com>
2015-04-02 21:20:41 +0300

    Removed failing test. Added placeholder for tests.

---
+++ test/clj_formal_specifications_app/spec_test.clj
@@ -0,0 +1,7 @@
+(ns clj-formal-specifications-app.spec-test
+  (:require [clojure.test :refer :all]
+            [clj-formal-specifications-app.spec :refer :all]))
+
+(deftest a-test
+  (testing "TODO, tests."
+    (is (true? true))))
